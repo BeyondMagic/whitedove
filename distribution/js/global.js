dad404bb98c1a1854dfdf3b759dd9ad90ab2affc
J.Farias (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2022-10-20 17:36:57 -0300

    LOG-OFF: distribution.

---
+++ distribution/js/global.js
@@ -0,0 +1,6 @@
+"use strict";
+(() => {
+  // source/ts/global.ts
+  window.Neutralino.init();
+  console.log("woooooooowaaaaaa");
+})();
---
+++ distribution/js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+(() => {
+  // source/ts/main.ts
+  console.log("Halloool!");
+})();
