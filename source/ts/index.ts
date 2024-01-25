0db7a62f8ce592edc3c1e5cbe5008bc201a70110
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-01-25 03:30:30 -0300

    whitedove: remaking this!

--- source/ts/index.ts
+++
@@ -1,7 +0,0 @@
-import * as System from './modules/System'
-//import * as Apresentation from './modules/Apresentation'
-import * as Inter from './modules/UI/Interface'
-
-System.initialisation()
-Inter.select('home')
-//Apresentation.start()
