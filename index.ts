777881332329e69049c4174913570fc3132ce2f0
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-01-25 22:07:59 -0300

    setup: initial project

---
+++ index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env bun
+//
+// Dependencies:
+//	bun
+//	dart-sass
+//
+// João Farias © 2021-2024 BeyondMagic <beyondmagic@mail.ru>
+
+import { withHtmlLiveReload as Serve } from "bun-html-live-reload";
+import root from "./source/html/root.html";
+import css from "./distribution/main.css";
+
+const SCSS = Bun.spawn([
+	"sass",
+	"--watch",
+	"--no-source-map",
+	"./source/scss/main.scss",
+	"./distribution/main.css",
+]);
+
+
+export default Serve({
+	port: 1984,
+	hostname: "0.0.0.0",
+	fetch: (req) => {
+		const url = new URL(req.url);
+
+		if (url.pathname === "/main.css")
+			return new Response(Bun.file(css), {
+				headers: { "Content-Type": "text/css" },
+			});
+
+		return new Response(Bun.file(root), {
+			headers: { "Content-Type": "text/html" },
+		});
+	},
+});
---
+++ scss.ts
@@ -0,0 +1,10 @@
+// João Farias © 2021-2024 BeyondMagic <beyondmagic@mail.ru>
+
+import type { BunPlugin } from "bun";
+
+const myPlugin: BunPlugin = {
+  name: "SCSS loader.",
+  setup(build) {
+    // implementation
+  },
+};
