22a4bfe2536a1dfad803df0794d6056a1ef12210
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-01-28 05:34:37 -0300

    source: add line namespace and first breakline strategy

--- source/main.ts
+++ source/main.ts
@@ -1,13 +1,36 @@
 // João Farias © 2024 BeyondMagic <beyondmagic@mail.ru>
 
+namespace Line {
+	export function create() {
+		const line = document.createElement('section')
+
+		line.contentEditable = 'true'
+		line.classList.add('line')
+
+		line.onkeydown = event => {
+			switch (event.key) {
+				case 'Enter':
+					const range = window.getSelection()?.getRangeAt(0)
+					if (range && (!range.startOffset && !range.endOffset))
+						line.before(create())
+					else
+						line.after(create())
+					event.preventDefault()
+					break
+				default:
+					break
+			}
+		}
+
+		return line
+	}
+}
+
 function create() {
 	const editor = document.createElement('main')
 	editor.classList.add('editor')
 
-	const line = document.createElement('section')
-	line.contentEditable = 'true'
-
-	editor.append(line)
+	editor.append(Line.create())
 
 	return editor
 }
