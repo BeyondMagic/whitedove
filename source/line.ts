b74cadae0e2f38b633a9a320e9248d80ea3eae16
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-01-28 22:52:18 -0300

    source: add core plugins, making editor more modular

--- source/line.ts
+++ source/line.ts
@@ -1,54 +1,25 @@
 import * as Selection from "./selection"
+import * as Plugin from "./plugin"
+
+export type Parameters = {
+	plugins : Plugin.Plugin[]
+}
+
 // Function: Create line with content and return its element.
-export function create() {
+export function create({
+	plugins,
+} : Parameters) {
 	const line = document.createElement('section')
 
 	line.contentEditable = 'true'
 	line.classList.add('line')
 
-	line.onkeydown = event => {
-		const selection = window.getSelection()
-		if (!selection)
-			throw Error("Selection not found.")
-
-		const range = selection.getRangeAt(0)
-		if (!range)
-			throw Error("Range not found.")
 	const selection = Selection.get()
 
-		const text = line.textContent ?? ''
-		console.log(event)
-
-		switch (event.key) {
-			case 'Enter':
-				event.preventDefault()
-				{
-					const new_line = create()
-
-					// TODO: only Caret.
-					switch (range.endOffset)
-					{
-						// Start of the line.
-						case 0:
-							line.before(new_line)
-							break;
-
-						// End of the line
-						case text.length:
-							line.after(new_line)
-							new_line.focus()
-							break;
-
-						// Between the extreme points of the line.
-						default:
-							new_line.textContent = text.substring(range.endOffset, text.length)
-							line.after(new_line)
-							line.textContent = text.substring(0, range.endOffset)
-							new_line.focus()
-					}
-				}
-				break
-		}
+	line.onkeydown = event => {
+		for (const plugin of plugins)
+			if (plugin.onkeydown)
+				plugin.onkeydown({ plugins, source: line, selection, event })
 	}
 
 	return line
--- source/main.ts
+++ source/main.ts
@@ -1,12 +1,36 @@
 // João Farias © 2024 BeyondMagic <beyondmagic@mail.ru>
 
 import * as Line from "./line"
+import * as Plugin from "./plugin"
+import * as Core from "./core"
 
-function create() {
+type Parameters = {
+	// The plugins for 
+	plugins? : Plugin.Plugin[]
+
+	// Whether to add the core plugins or not.
+	core?: boolean
+}
+
+function create({
+	core,
+	plugins,
+} : Parameters = {
+	core: true,
+	plugins: [],
+}) {
+	core = core ?? true
+	plugins = plugins ?? []
+
+	if (core)
+		plugins.unshift({
+			onkeydown: Core.keydown
+		})
+
 	const editor = document.createElement('main')
 	editor.classList.add('editor')
 
-	const line = Line.create()
+	const line = Line.create({ plugins })
 	editor.append(line)
 	line.focus()
 
